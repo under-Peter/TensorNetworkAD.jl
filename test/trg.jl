14d218b053bb90a9b548a2acee8737c436889515
GiggleLiu <cacate0129@gmail.com>
2019-06-06 14:08:34 +0800

    add more tests about result

--- test/trg.jl
+++ test/trg.jl
@@ -1,5 +1,19 @@
+using TensorNetworkAD
+using Test
 using Zygote
+
 @testset "trg" begin
-    foo = x -> trg(x,5,5)
+    χ = 5
+    niter = 5
+    foo = x -> trg(x, χ, niter)
+    @test foo(0.4)/2^niter ≈ 0.8919788686747141  # the pytorch result
+    @test num_grad(foo, 0.4, δ=1e-6) ≈ Zygote.gradient(foo, 0.4)[1]
+end
+
+@testset "trg" begin
+    χ = 5
+    niter = 5
+    foo = x -> trg(x, χ, niter)
+    @test foo(0.4)/2^niter ≈ 0.8919788686747141  # the pytorch result
     @test num_grad(foo, 0.4, δ=1e-6) ≈ Zygote.gradient(foo, 0.4)[1]
 end

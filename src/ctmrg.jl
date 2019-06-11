9e93c7704cb075d673aeca1c47d9f97a61b35253
Andreas Peter <andreas.peter.ch@gmail.com>
2019-06-11 15:15:39 +0200

    Fix AD in ctmrg

--- src/ctmrg.jl
+++ src/ctmrg.jl
@@ -1,42 +1,36 @@
 using LinalgBackwards, OMEinsum
-using LinearAlgebra: normalize
+using LinearAlgebra: normalize, norm
+using Random
+
+initializec(a, χ) = rand(eltype(a), χ, χ)
+initializet(a, χ) = rand(eltype(a), χ, size(a,1), χ)
+@Zygote.nograd initializec, initializet
+
 function ctmrg(a, χ, tol, maxit::Integer)
-    #=
-         |3
-      4-[a]-2
-         |1
-
-     [c]-2
-      |
-      1
-
-       |3
-      [t]-2
-       |1
-    =#
     d = size(a,1)
     # initialize
-    c = rand(eltype(a), χ, χ)
-    t = rand(eltype(a), χ, d, χ)
+    c = initializec(a, χ)
+    t = initializet(a, χ)
 
     # symmetrize
     c += transpose(c)
     t += permutedims(t, (3,2,1))
-    oldsvdvals = fill(Inf, χ)
+    oldsvdvals = Inf .* ones(χ)
     vals = copy(oldsvdvals)
+    diff = Inf
 
     for i in 1:maxit
         # grow
-        cp = meinsum(((-1,-2),(1,-3,-1),(-2,-4,4),(2,3,-4,-3)),
+        cp = einsum(((-1,-2),(1,-3,-1),(-2,-4,4),(2,3,-4,-3)),
             (c, t, t, a), (1,2,3,4))
-        tp = meinsum(((1,-1,5), (3,4,-1,2)), (t,a), (1,2,3,4,5))
+        tp = einsum(((1,-1,5), (3,4,-1,2)), (t,a), (1,2,3,4,5))
 
         # renormalize
-        cpmat = reshape(cp, prod(size(cp)[1:2]), prod(size(cp)[3:4]))
+        cpmat = reshape(cp, size(cp,1) * size(cp,2), size(cp,3) * size(cp,4))
         u, s, v = svd(cpmat)
         z = reshape(u[:, 1:χ], χ, d, χ)
-        c = meinsum(((-1,-2,-3,-4), (-1,-2,1), (-4,-3,2)), (cp, z, conj(z)), (1,2))
-        t = meinsum(((-1,-2,2,-3,-4), (-1,-2,1), (-4,-3,3)), (tp, conj(z), z), (1,2,3))
+        c = einsum(((-1,-2,-3,-4), (-1,-2,1), (-4,-3,2)), (cp, z, conj(z)), (1,2))
+        t = einsum(((-1,-2,2,-3,-4), (-1,-2,1), (-4,-3,3)), (tp, conj(z), z), (1,2,3))
 
         # symmetrize
         c +=  transpose(c)
@@ -46,11 +40,11 @@ function ctmrg(a, χ, tol, maxit::Integer)
         _, vals, = svd(c)
         maxval = maximum(vals)
         c = c ./ maxval
-        t = t ./ meinsum(((1,2,3),), (t,), ())
-        vals = normalize(vals,1)
+        t = t ./ einsum(((1,2,3),), (t,), ())[1]
+        vals = vals ./ norm(vals,1) #normalize(vals,1)
 
         #compare
-        sum(abs, oldsvdvals - vals) < tol && return (c, t, vals)
+        diff = sum(abs, oldsvdvals - vals)
         oldsvdvals = vals
     end
     return (c, t, vals)
--- test/ctmrg.jl
+++ test/ctmrg.jl
@@ -3,35 +3,44 @@ using Test, Random
 using Zygote
 using OMEinsum
 
+@Zygote.adjoint function Base.typed_hvcat(::Type{T}, rows::Tuple{Vararg{Int}}, xs::S...) where {T,S}
+  Base.typed_hvcat(T,rows, xs...), ȳ -> (nothing, nothing, permutedims(ȳ)...)
+end
+
+
 function isingtensor(β)
-    a = zeros(Float64,2,2,2,2)
-    a[1,1,1,1] = a[2,2,2,2] = 1
+    a = reshape(Float64[1 0 0 0; 0 0 0 0; 0 0 0 0; 0 0 0 1], 2,2,2,2)
     cβ, sβ = sqrt(cosh(β)), sqrt(sinh(β))
     q = 1/sqrt(2) * [cβ+sβ cβ-sβ; cβ-sβ cβ+sβ]
-    a = einsum(((-1,-2,-3,-4), (-1,1), (-2,2), (-3,3), (-4,4)), (a,q,q,q,q), (1,2,3,4))
-    return a
+    einsum(((-1,-2,-3,-4), (-1,1), (-2,2), (-3,3), (-4,4)), (a,q,q,q,q), (1,2,3,4))
 end
 
 function isingmagtensor(β)
-    a = zeros(Float64,2,2,2,2)
-    a[1,1,1,1] = 1
-    a[2,2,2,2] = -1
+    a = reshape([1 0 0 0; 0 0 0 0; 0 0 0 0; 0 0 0 -1] , 2,2,2,2)
     cβ, sβ = sqrt(cosh(β)), sqrt(sinh(β))
     q = 1/sqrt(2) * [cβ+sβ cβ-sβ; cβ-sβ cβ+sβ]
-    a = einsum(((-1,-2,-3,-4), (-1,1), (-2,2), (-3,3), (-4,4)), (a,q,q,q,q), (1,2,3,4))
-    return a
+    einsum(((-1,-2,-3,-4), (-1,1), (-2,2), (-3,3), (-4,4)), (a,q,q,q,q), (1,2,3,4))
 end
 
 function magnetisationofβ(β, χ)
     a = isingtensor(β)
     m = isingmagtensor(β)
-    c, t, = ctmrg(a, χ, 1e-6, 100)
+    c, t, = TensorNetworkAD.ctmrg(a, χ, 1e-6, 100)
     ctc = einsum(((1,-1),(-1,2,-2),(-2,3)), (c,t,c), (1,2,3))
     env = einsum(((-1,4,-3),(-3,3,-4),(-2,2,-4),(-2,1,-1)),(ctc,t,ctc,t),(1,2,3,4))
     mag = einsum(((1,2,3,4),(1,2,3,4)), (env,m),())[]
     norm = einsum(((1,2,3,4),(1,2,3,4)), (env,a),())[]
 
     return abs(mag/norm)
+end
+
+function eoeofβ(β,χ)
+    a = isingtensor(β)
+    m = isingmagtensor(β)
+    c, t, = TensorNetworkAD.ctmrg(a, χ, 1e-6, 100)
+    d = diag(c)
+    d = d ./ norm(d,1)
+    return -sum( d .* log2.(d))
 end
 
 function magofβ(β)
@@ -52,9 +61,12 @@ end
     @test magnetisationofβ(0.6,4) ≈ magofβ(0.6)
     @test magnetisationofβ(0.8,2) ≈ magofβ(0.8)
 
-    Random.seed!(2)
-    χ = 5
-    niter = 5
+    foo = x -> magnetisationofβ(x,2)
+    @test Zygote.gradient(foo,0.5)[1] ≈ num_grad(foo,0.5)
+
+    # Random.seed!(2)
+    # χ = 5
+    # niter = 5
     # $ python 2_variational_iPEPS/variational.py -model TFIM -D 3 -chi 10 -Niter 10 -Nepochs 10
-    @test_broken isapprox(ctmrg(:TFIM; nepochs=10, χ=10, D=3, niter=10).E, -2.1256619, atol=1e-5)
+    # @test_broken isapprox(ctmrg(:TFIM; nepochs=10, χ=10, D=3, niter=10).E, -2.1256619, atol=1e-5)
 end

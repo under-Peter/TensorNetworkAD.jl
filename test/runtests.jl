abd5af4d1eed9459d7d564c6a5d0b59f59b6497b
Andreas Peter <andreas.peter.ch@gmail.com>
2019-06-26 16:01:45 +0200

    Add progressmeter for information during runtest

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,14 @@
 using TensorNetworkAD
 using Test
+using ProgressMeter
+p = ProgressUnknown("Test run:")
 
 @testset "TensorNetworkAD.jl" begin
+    next!(p)
     include("trg.jl")
+    next!(p)
     include("ctmrg.jl")
+    next!(p)
     include("variationalipeps.jl")
+    finish!(p)
 end

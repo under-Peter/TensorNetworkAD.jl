c4f232051d79f2624aa7e77c15800ee561ddd596
GiggleLiu <cacate0129@gmail.com>
2019-08-19 01:26:48 +0800

    fix test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,14 +2,6 @@ using TensorNetworkAD
 using Test
 using Random
 
-Random.seed!(4)
-include("autodiff.jl")
-include("exampletensors.jl")
-include("fixedpoint.jl")
-include("trg.jl")
-include("ctmrg.jl")
-include("variationalipeps.jl")
-
 @testset "TensorNetworkAD.jl" begin
     println("autodiff tests running...")
     include("autodiff.jl")
